42145ed7dd065c25edbd44136708cfadfd6ce390
Aaron Petkau <aaron.petkau@canada.ca>
2021-09-07 17:36:51 -0500

    Added unset case

--- evaluations/sars-cov-2/3-index-genomes.sh
+++ evaluations/sars-cov-2/3-index-genomes.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #for case_file in case*.yaml;
-for case_file in case-10.yaml case-20.yaml case-50.yaml;
+#for case_file in case-10.yaml case-20.yaml case-50.yaml;
+for case_file in case-100.yaml case-200.yaml case-500.yaml;
 do
-	name=`basename $simulation .yaml`
+	name=`basename $case_file .yaml`
 	d=`date | tr -d '\n'`
 	echo "$d: analysis for $case_file"
 	papermill template-3-index-genomes.ipynb 3-index-genomes.${name}.ipynb -f $case_file
