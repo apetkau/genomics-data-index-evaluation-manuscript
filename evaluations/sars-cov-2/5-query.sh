ffab67b77102534431ad48628845a7968a81653e
Aaron Petkau <aaron.petkau@canada.ca>
2021-09-07 17:17:02 -0500

    Fixed up template and query script

---
+++ evaluations/sars-cov-2/5-query.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for case_file in case*.yaml;
+do
+	name=`basename $case_file .yaml`
+	d=`date | tr -d '\n'`
+	echo "$d: query comparison $case_file"
+	papermill template-5-query.ipynb 5-query.${name}.ipynb -f $case_file
+done
