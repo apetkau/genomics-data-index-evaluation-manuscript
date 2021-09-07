bea4ea905683120a9393ca53cd4d360bf946daff
Aaron Petkau <aaron.petkau@canada.ca>
2021-09-07 17:47:20 -0500

    Fixed incorrect rename

--- evaluations/sars-cov-2/4-query.sh
+++ evaluations/sars-cov-2/4-query.sh
@@ -5,5 +5,5 @@ do
 	name=`basename $case_file .yaml`
 	d=`date | tr -d '\n'`
 	echo "$d: query comparison $case_file"
-	papermill template-5-query.ipynb 4-query.${name}.ipynb -f $case_file
+	papermill template-4-query.ipynb 4-query.${name}.ipynb -f $case_file
 done
