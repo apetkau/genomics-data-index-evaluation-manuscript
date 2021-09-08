2fa3361a2ca89212544b383199226f1ecf701634
Aaron Petkau <aaron.petkau@canada.ca>
2021-09-07 23:52:37 -0500

    Added additional cases

--- evaluations/sars-cov-2/4-query.sh
+++ evaluations/sars-cov-2/4-query.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for case_file in case-10.yaml case-20.yaml case-50.yaml case-100.yaml case-200.yaml case-500.yaml case-1000.yaml case-2000.yaml case-5000.yaml case-10000.yaml;
+#for case_file in case-10.yaml case-20.yaml case-50.yaml case-100.yaml case-200.yaml case-500.yaml case-1000.yaml case-2000.yaml case-5000.yaml case-10000.yaml;
+#for case_file in case-10.yaml case-20.yaml case-50.yaml;
+for case_file in case-100.yaml;
 do
 	name=`basename $case_file .yaml`
 	d=`date | tr -d '\n'`
