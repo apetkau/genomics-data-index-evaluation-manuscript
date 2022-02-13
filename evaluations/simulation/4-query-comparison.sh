2eba33f70e00a2e19937aa998215b6618ef2c22f
Aaron Petkau <aaron.petkau@canada.ca>
2022-02-12 18:38:50 -0600

    Logging output in papermill

--- evaluations/simulation/4-query-comparison.sh
+++ evaluations/simulation/4-query-comparison.sh
@@ -5,5 +5,5 @@ do
 	name=`basename $simulation .yaml`
 	d=`date | tr -d '\n'`
 	echo "$d: query comparison $simulation"
-	papermill template-5-query.ipynb 5-query.${name}.ipynb -f $simulation
+	papermill --log-output template-5-query.ipynb 5-query.${name}.ipynb -f $simulation
 done
