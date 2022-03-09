e80fefc64ce2e37842c56aacdd4f17b6a8fc0a32
Aaron Petkau <aaron.petkau@canada.ca>
2022-03-09 15:41:23 -0600

    Added new simulation cases

--- evaluations/simulation/1-data-simulation.sh
+++ evaluations/simulation/1-data-simulation.sh
@@ -5,7 +5,7 @@ do
 	name=`basename $simulation .yaml`
 	d=`date | tr -d '\n'`
 	echo "$d: simulating reads for $simulation"
-	papermill template-1-simulate-reads.ipynb 1-simulate-reads.${name}.ipynb -f $simulation
+	papermill --log-output template-1-simulate-reads.ipynb 1-simulate-reads.${name}.ipynb -f $simulation
 	echo "$d: fixing reads for $simulation"
-	papermill template-2-fix-simulated-files.ipynb 2-fix-simulated-files.${name}.ipynb -f $simulation
+	papermill --log-output template-2-fix-simulated-files.ipynb 2-fix-simulated-files.${name}.ipynb -f $simulation
 done
