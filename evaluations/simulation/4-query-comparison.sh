2b954138886e71760bbd04f6931c6561c4395e5f
Aaron Petkau <aaron.petkau@canada.ca>
2022-10-18 16:59:32 -0500

    Removed query comparisons

--- evaluations/simulation/4-query-comparison.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for simulation in simulation*.yaml;
-do
-	name=`basename $simulation .yaml`
-	d=`date | tr -d '\n'`
-	echo "$d: query comparison $simulation"
-	papermill --log-output template-5-query.ipynb 5-query.${name}.ipynb -f $simulation
-done
